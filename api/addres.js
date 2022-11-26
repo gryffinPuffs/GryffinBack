abb2a87e828749f61f7b652029d3f4104cfa584a
ADMIN <admin@ADMINs-Mac-mini.local>
2022-11-26 16:32:23 -0600

    address api done, but not tested, and needs review

--- api/addres.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const addressRouter = express.Router();
-
-addressRouter.use((req, res, next) => {
-  console.log("A request is being made to /address");
-  next();
-});
-
-module.exports = addressRouter;
