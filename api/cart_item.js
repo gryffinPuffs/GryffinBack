1539f61e50c6dbc300365d290fef4f28dd72f919
Randy Pellegrin <rpell391@gmail.com>
2022-11-28 10:38:28 -0600

    corrections to cart_item patch and delete

--- api/cart_item.js
+++ api/cart_item.js
@@ -2,66 +2,61 @@ const express = require("express");
 const { getCartItemById, destroyItemInCart } = require("../db/cart_item");
 const { requireUser } = require("./utils");
 const cart_itemRouter = express.Router();
-const{
-  getCartItemById,
-} = require("../db")
+const { getCartItemById } = require("../db");
 
 cart_itemRouter.patch("/:cart_id", requireUser, async (req, res, next) => {
+  const { cart_id } = req.params;
+  const { product_id, price, quantity } = req.body;
+
+  const updateFields = {};
+
+  if (product_id) {
+    updateFields.product_id = product_id;
+  }
+  if (price) {
+    updateFields.price = price;
+  }
+  if (quantity) {
+    updateFields.quantity = quantity;
+  }
   try {
-    const { cart_id } = req.params;
-    const { product_id, price, quantity } = req.body
+    const originalCart = await getCartItemById(cart_id);
+    if (originalCart.cart_id === req.user.id) {
+      const updatedCart = await updateCart({
+        id: cart_id,
+        product_id,
+        price,
+        quantity,
+      });
 
-    const updateFields = {}
-
-    if (product_id) {
-      updateFields.product_id = product_id
-    }
-    if (price){
-      updateFields.price = price
-    }
-    if (quantity){
-      updateFields.quantity = quantity
-    }
-    try {
-      const originalCart = await 
-      getCartItemById(cart_id);
-      if (originalCart.cart_id = await 
-        updateCart({
-          id: cart_id,
-          product_id,
-          price,
-          quantity
-        }))
-        
-        res.send(updateCart)
-    }else {
+      res.send(updatedCart);
+    } else {
       next({
         name: "unauthorizedUserError",
         message: `user ${req.user.username} is not allowed to update ${originalCart})
         `,
-      })
+      });
     }
-  } catch(error) {
+  } catch (error) {
     throw error;
   }
 });
 
 cart_itemRouter.delete("/:cart_id", requireUser, async (req, res, next) => {
   try {
-    const cart = await getCartItemById(req.params.cart_id)
+    const cart = await getCartItemById(req.params.cart_id);
 
     if (cart && cart.user_id === req.user.id) {
-      const deleteCartItem = await destroyItemInCart(cart)
+      const deleteCartItem = await destroyItemInCart(cart);
       res.send(deleteCartItem);
-
-    } else{
+    } else {
       next({
         name: "unauthorizedUserError",
-        message: `user ${req.user.username} is not allowed to delete ${cart.name}`
-      })
+        message: `user ${req.user.username} is not allowed to delete ${cart.name}`,
+      });
     }
-  } catch ({name, message}) {
-    next({name, message});
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
 module.exports = cart_itemRouter;
